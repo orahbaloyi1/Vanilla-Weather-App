93872243ee1672543753c1d35c099045a6edf110
orahbaloyi1 <orahbaloyi1@gmail.com>
2023-09-22 21:12:31 +0200

    Updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
+function formDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 /*Axios will call this function and will get the response from the api*/
 function displayTemperature(response) {
   console.log(response.data);
@@ -5,15 +30,16 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-  let precipitationElement = document.querySelector("#precipitation");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
-  precipitationElement.innerHTML = response.data.main.pressure;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = response.data.wind.speed;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formDate(response.data.dt * 1000);
 }
 
 let apiKey = "7b2103381278d28203d89c397e41d56e";
